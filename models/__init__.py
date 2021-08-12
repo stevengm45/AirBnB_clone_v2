039f0712dffeeab66fef2b185ee56da2c07641a2
stevengm45 <stevengm45@gmail.com>
2021-08-11 21:22:12 -0500

    change in model.engine.filestorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This module instantiates an object of class FileStorage
-"""
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv as env
